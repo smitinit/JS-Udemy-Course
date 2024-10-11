37aeaa6c3b1adf5c31c500165e042a817c408607
Smit Patel <patelsmit090305@gmail.com>
2024-10-11 17:22:43 +0530

    midway

--- PROJECTS/Project_5(Advance DOM)/script.js
+++ PROJECTS/Project_5(Advance DOM)/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 ///////////////////////////////////////
+
 // Modal window
 
 const modal = document.querySelector('.modal');
@@ -8,7 +9,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +20,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +30,64 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  // const slcords = section1.getBoundingClientRect();
+  // console.log(e.target.getBoundingClientRect());
+
+  // window.scrollTo(
+  //   slcords.left + window.pageXOffset,
+  //   slcords.top + window.pageYOffset
+  // );
+  // window.scrollTo({ ///old school
+  //   left: slcords.left + window.pageXOffset,
+  //   top: slcords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' }); //new
+});
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.color = 'red';
+});
+
+// const h1 = document.querySelector('h1');
+// h1.addEventListener('mouseenter', e => {
+//   alert('sdfds');
+// });
+
+/*
+
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+// console.log(document.getElementsByTagName('button'));
+
+const message = document.createElement('div');
+const header = document.querySelector('.header');
+
+message.classList.add('cookie-message');
+// message.textContent = `We use cookies for improvements!` //method 1
+message.innerHTML = `We use cookies for improvements!. <button class = 'btn btn--close-cookie'>Got it!</button>`; //method 2
+
+header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => message.parentElement.removeChild(message)); //old school
+// .addEventListener('click', () => message.remove()); //new method ez one
+
+message.style.backgroundColor = '#37383d';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+
+*/
