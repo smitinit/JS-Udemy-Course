e0bffa4e95842b1c360b5282b2573b1708824ea3
Smit Patel <patelsmit090305.com>
2024-10-12 15:44:50 +0530

    js oop

--- SESSIONS/index.oop.js
+++ SESSIONS/index.oop.js
@@ -1,3 +1,44 @@
 'use strict';
+//                     OOP                         //
+// THIS HAPPENS BEHIND THE SCENES //
+//1. new {} is created
+//2. function is called, this ={}
+//3. {} linked to prototype
+//4. funtion auto returns {}
 
-console.log('hi');
+const Person = function (f, b) {
+  this.f = f;
+  this.b = b;
+
+  //NEVER DO THIS
+  // this.a = function () {
+  //   console.log(2024 - this.b);
+  // };
+};
+
+const smit = new Person('Smit', 2005);
+const alice = new Person('Alice', 1990);
+const jack = new Person('Jack', 2000);
+
+console.log(smit, alice, jack); //instances of Person
+console.log(smit instanceof Person); //true
+
+//prototype inheritance
+//method
+Person.prototype.calcAge = function () {
+  console.log(2024 - this.b);
+};
+
+smit.calcAge();
+alice.calcAge();
+jack.calcAge();
+
+console.log(smit.__proto__);
+console.log(smit.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(smit));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+//properties
+
+Person.prototype.ishuman = true;
+console.log(smit.ishuman, alice.ishuman, jack.ishuman);
